ccdade8c50b3d838b151e8eeabee5998980cd3ae
parthkothwala1811@gmail.com <parthkothwala1811@gmail.com>
2024-07-17 23:25:23 +0530

    all filed added

---
+++ razorpay.js
@@ -0,0 +1,12 @@
+let check=document.getElementById("payment-box");
+function payment_box()
+{
+    if(check.hidden)
+    {
+        check.hidden=false;
+    }
+    else
+    {
+        check.hidden=true;
+    }
+}
